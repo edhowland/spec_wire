90dcb75945ee4d484d8823058c450ccf510311c6
Ed Howland <ed.howland@gmail.com>
2010-01-07 12:19:17 -0500

    added gem_spec.rb

---
+++ spec/gem_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+describe "Gem" do
+  describe "loading const module" do
+    it "should have an attribute we can check somehow" do
+      # ruby code to detect if a file has been loaded
+    end
+  end
+end
