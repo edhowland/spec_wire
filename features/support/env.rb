2c277c720b89082752402fdb53f00fa60eb8f43c
Ed Howland <ed.howland@gmail.com>
2010-02-03 16:13:49 -0500

    added features and templates

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
-require 'spec_on'
+require 'spec_wire'
 
 require 'spec/expectations'
+
+SpecWire::Initializer.run do |config|
+  config.server_url = 'http://localhost/~edh/limonade/server.php'
+  config.session_key = 'Fresh_and_Minty_Limonade_App'
+  config.put_needs_method_arg = true 
+  config.class_cache = 'tmp/class_cache'
+end
